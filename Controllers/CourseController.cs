fc31f2e5f5ca7276831f8f89a610f975d35608c1
Seyit Ahmet Çilingir <seyitacilingir@gmail.com>
2024-06-25 11:08:41 +0300

    project has been complated

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -1,12 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
+using Academy.Models;
 
-namespace Academy.Controllers{
-    public class CourseController: Controller{
-        public IActionResult Index(){
+namespace Academy.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = Repository.Applications;
+            return View(model);
+        }
+
+        public IActionResult Apply()
+        {
             return View();
         }
 
-        public IActionResult Apply(){
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Apply([FromForm] Candidate model)
+        {
+            if(Repository.Applications.Any(c => c.Email.Equals(model.Email)))
+            {
+                ModelState.AddModelError("","Böyle bir kayıt zaten var");
+            }
+
+            if (ModelState.IsValid)
+            {
+                Repository.Add(model);
+                return View("Feedback", model);
+            }
             return View();
         }
     }
--- Models/Candidate.cs
+++ Models/Candidate.cs
@@ -1,10 +1,20 @@
+
+
+using System.ComponentModel.DataAnnotations;
+
 namespace Academy.Models{
+
+    
     public class Candidate{
+        [Required(ErrorMessage = "Email gereklidir.")]
         public String? Email { get; set; } = String.Empty;
+        [Required(ErrorMessage = "Ad gereklidir.")]
         public String? FirstName { get; set; } = String.Empty;
+        [Required(ErrorMessage = "Soyisim gereklidir.")]
         public String? LastName { get; set; } = String.Empty;
         public String FullName => $"{FirstName} {LastName?.ToUpper()}";
         public int? Age { get; set; } 
+        [Required(ErrorMessage = "Kurs gereklidir.")]
         public String? SelectedCourse { get; set; } = String.Empty;
         public DateTime ApplyAt { get; set; }
 
